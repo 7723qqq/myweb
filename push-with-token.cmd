18fce247f0bdd8822d5fefa2ea7e60059a02a844
你的名字 <@example.com>
2026-04-04 03:59:58 +0800

    清理仓库：删除所有文件

--- push-with-token.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set TOKEN=11APTP2AA0SDgAUQUI8TB7_1Nfh3MGffsERn5Cu355CNegcP3cH8oEfnUWMGlbBYiZLQLHXOLO24DC2rR7
-set GITHUB_USER=7723qqq
-git remote set-url origin https://%GITHUB_USER%:%TOKEN%@github.com/%GITHUB_USER%/myweb.git
-git push -u origin master 2>&1 | findstr /V "Invalid username"
-endlocal
